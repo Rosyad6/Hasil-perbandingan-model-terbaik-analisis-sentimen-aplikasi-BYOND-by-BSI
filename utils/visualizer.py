a12efc8f9191a6cadb9cb7480b5273ba243c4ca5
Rosyad6 <raflyrosyad123@gmail.com>
2026-07-30 11:28:12 +0700

    Enhance sentiment pie chart appearance
    Adjust pie chart size and update text color for dark background.

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 
 
 def create_sentiment_pie(sentiment_count):
-
+    # Mengecilkan sedikit ukuran jika dirasa masih kebesaran
     fig, ax = plt.subplots(figsize=(3, 3))
+    
+    # Membuat latar belakang figure transparan
+    fig.patch.set_alpha(0.0)
+    ax.patch.set_alpha(0.0)
 
-    ax.pie(
+    # Mengubah warna teks agar terlihat di background gelap
+    wedges, texts, autotexts = ax.pie(
         sentiment_count,
         labels=sentiment_count.index,
         autopct="%1.1f%%",
-        startangle=90
+        startangle=90,
+        textprops={'color': "white"} # Ubah warna teks label
     )
 
-    ax.set_title("Distribusi Sentimen")
+    # Mengatur warna judul
+    ax.set_title("Distribusi Sentimen", color="white")
 
     return fig
+    
