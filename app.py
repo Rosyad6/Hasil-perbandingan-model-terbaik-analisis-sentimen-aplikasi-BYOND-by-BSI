1314429ea05376059d76d61a775b21d25c5dcb0d
Rosyad6 <raflyrosyad123@gmail.com>
2026-07-31 15:46:32 +0700

    Update logo image width in app.py
    Increase logo image width from 150 to 170 pixels.

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ col_logo, col_title = st.columns([1, 5])
 
 with col_logo:
     try:
-        st.image("assets/logo.png", width=150)
+        st.image("assets/logo.png", width=170)
     except Exception:
         pass  # Mencegah error jika gambar belum ada/lokasi tidak ditemukan
 
